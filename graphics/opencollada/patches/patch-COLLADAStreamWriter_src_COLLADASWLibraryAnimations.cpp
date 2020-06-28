b90c054620aa99f7a21bc5088d5c2763b338c745
Gagandeep Sawant <grsawant@posteo.net>
2020-06-29 00:26:48 +0530

    opencollada: drop explicit initialization of mOpenAnimations to fix build

---
+++ graphics/opencollada/patches/patch-COLLADAStreamWriter_src_COLLADASWLibraryAnimations.cpp
@@ -0,0 +1,15 @@
+$OpenBSD$
+
+drop explicit initialization of mOpenAnimations to fix build
+
+--- COLLADAStreamWriter/src/COLLADASWLibraryAnimations.cpp.orig	2017-05-10 01:36:49 UTC
++++ COLLADAStreamWriter/src/COLLADASWLibraryAnimations.cpp
+@@ -62,7 +62,7 @@ namespace COLLADASW
+ 
+     //---------------------------------------------------------------
+     LibraryAnimations::LibraryAnimations ( COLLADASW::StreamWriter * streamWriter )
+-            : Library ( streamWriter, CSWC::CSW_ELEMENT_LIBRARY_ANIMATIONS ), mOpenAnimations ( NULL )
++            : Library ( streamWriter, CSWC::CSW_ELEMENT_LIBRARY_ANIMATIONS )
+     {}
+ 
+     //---------------------------------------------------------------
